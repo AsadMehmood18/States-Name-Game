a1e60f928a219831d37a8530b416ab54c61076bf
Asad Mehmood <bcpe223053@cust.pk>
2024-09-06 23:24:10 +0500

    Minor Improvements

--- main.py
+++ main.py
@@ -17,10 +17,7 @@ while len(guessed_states)<50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
 
     if answer_state == "Exist":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         data_dic = {
             "states": missing_states
         }
